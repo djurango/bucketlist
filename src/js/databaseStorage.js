52fdd51f6117ef5d03b86b9fbf7aa5bd333644e0
djurango <djuric@me.com>
2019-06-27 11:52:57 +0200

    storage

--- src/js/databaseStorage.js
+++ src/js/databaseStorage.js
@@ -5,7 +5,7 @@ DatabaseStorage.SERVER = 'http://127.0.0.1:1337';
 
 DatabaseStorage.getList = function(){
 
-    $.get(DatabaseStorage.SERVER+'/get', function( data ) {
+    $.get('/get', function( data ) {
 
         NoteList.list = JSON.parse(data)[0].storedList;
         NoteList.render();
